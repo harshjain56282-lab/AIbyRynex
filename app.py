456280e6ee3c976378b8edebb5fc1058a9f65794
RynexAI <harshjain56282@gmail.com>
2025-11-07 16:13:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,5 @@ def chat():
 
 if __name__ == "__main__":
     # Run in debug mode locally
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+
